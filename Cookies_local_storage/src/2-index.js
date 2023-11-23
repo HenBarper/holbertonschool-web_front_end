794a1f960cc9ac429d18d1775afcd4ab1dd45a92
henbarper <benjaminitonio@gmail.com>
2023-11-22 20:36:43 -0600

    2 v1

---
+++ Cookies_local_storage/src/2-index.js
@@ -0,0 +1,29 @@
+function setCookies() {
+    const expDate = new Date();
+    expDate.setDate(expDate.getDate() + 10);
+
+    document.cookie = `firstname=${document.querySelector('#firstname').value}`;
+    document.cookie = `email=${document.querySelector('#email').value}`;
+    document.cookie = `expires="${expDate}"`;
+}
+
+function showCookies() {
+    // let cookies = document.cookie;
+    let p = document.createElement('p');
+    let cookEmail = getCookie('email');
+    let cookName = getCookie('firstname');
+    p.innerHTML = `Email: ${cookEmail} - Firstname: ${cookName}`;
+    document.body.appendChild(p);
+}
+
+function getCookie(name) {
+    let cookies = document.cookie.split(';');
+
+    for(let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].trim();
+        if (cookie.startsWith(name + '=')) {
+            return cookie.substring(name.length + 1);
+        }
+    }
+    return '';
+}
