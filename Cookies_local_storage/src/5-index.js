38ce33586a4f2d73c5c45df63327c1abcac29b19
henbarper <benjaminitonio@gmail.com>
2023-11-22 21:42:42 -0600

    5 and 6

---
+++ Cookies_local_storage/src/5-index.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function () {
+    if (typeof(Storage) === "undefined") {
+        alert("Sorry, your browser does not support Web storage. Try again with a better one");
+        return;
+    }
+
+    const availableItems = ["Shampoo", "Soap", "Sponge", "Water"];
+
+    function addGrocery(item) {
+        localStorage.setItem(item, true);
+        displayCart();
+    }
+
+    function createStore() {
+        const ul = document.createElement('ul');
+
+        availableItems.forEach(item => {
+            const li = document.createElement('li');
+            li.textContent = item;
+
+            li.addEventListener('click', function () {
+                addGrocery(item);
+            });
+
+            ul.appendChild(li);
+        });
+
+        document.body.appendChild(ul);
+    }
+
+    function displayCart() {
+        const cartItems = Object.keys(localStorage);
+
+        if (cartItems.length === 0) {
+            return;
+        }
+
+        const p = document.createElement('p');
+        p.textContent = `You previously had ${cartItems.length} items in your cart`;
+        document.body.appendChild(p);
+    }
+
+    createStore();
+    displayCart();
+});
